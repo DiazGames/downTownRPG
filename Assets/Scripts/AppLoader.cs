9bb5d4bac22dbefbe567c15e6e5efed04f292c1e
Jacky Chang <jackychang99@qq.com>
2019-03-21 21:34:46 +0800

    UI界面

---
+++ Assets/Scripts/AppLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QFramework;
+
+namespace DiazDTRPG
+{
+    public class AppLoader : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ResMgr.Init();
+            UIMgr.SetResolution(Screen.width, Screen.height, 1.0f);
+            DontDestroyOnLoad(UIManager.Instance);
+        }
+
+        private void Start()
+        {
+            UIMgr.OpenPanel<UIHomePanel>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
